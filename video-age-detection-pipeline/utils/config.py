9e953b84fc4be2dffbede1b3a6a8322b1b89acfd
leela krishna <leela.krishna@centific.com>
2025-08-15 16:28:10 +0000

    updated config

--- video-age-detection-pipeline/utils/config.py
+++ video-age-detection-pipeline/utils/config.py
@@ -4,7 +4,7 @@ ENFORCE_DETECTION = False
 ACTIONS = ["age", "gender"]
 
 OUTPUT_DIR = "outputs"
-SAVE_FRAMES = True
+SAVE_FRAMES = False
 SAVE_ONLY_DETECTIONS = False
 
 WRITE_JSON = True      # <-- turn on nested JSON
